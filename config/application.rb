c7ca79680a848ec89394b1d64b926da33749e2ec
Peter Schilling <peter@peterschilling.org>
2016-10-09 13:00:37 -0700

    Render the partial from webpack

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,6 @@ module Webpackin
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.paths["app/views"] << Rails.root.join("public", "webpack", "views")
   end
 end
