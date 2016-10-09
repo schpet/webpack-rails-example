c1e1e6a616094850adf25a710b29ae598a492dd8
Peter Schilling <peter@peterschilling.org>
2016-10-09 14:12:28 -0700

    Support CSS in webpack

--- client/src/app.js
+++ client/src/app.js
@@ -1,1 +1,2 @@
+require('./styles.css')
 alert('howdy')
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,5 @@
 var HtmlWebpackPlugin = require("html-webpack-plugin")
+var ExtractTextPlugin = require("extract-text-webpack-plugin")
 
 module.exports = {
   entry: "./client/src/app.js",
@@ -7,7 +8,18 @@ module.exports = {
     publicPath: "/webpack/",
     filename: "bundle.[hash].js"
   },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+      }
+      // use this instead to have js generate a <style> tag:
+      // { test: /\.css$/, loader: "style-loader!css-loader" }
+    ]
+  },
   plugins: [
+    new ExtractTextPlugin("styles.[hash].css"),
     new HtmlWebpackPlugin({
       template: "./client/blank.html",
       filename: "views/_webpack_head.html"
